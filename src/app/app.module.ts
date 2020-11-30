b660ad3122f2850158f5764ff473107920e94109
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 15:08:58 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ArticulosComponent } from "./components/articulos/articulos.component";
 import { ArticulosFamiliasComponent } from "./components/articulos-familias/articulos-familias.component";
 import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.component";
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { ClientesService } from './services/clientes.service';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,7 @@ import { ClientesComponent } from './components/clientes/clientes.component';
   entryComponents: [ModalDialogComponent],
   providers: [
      {provide: APP_BASE_HREF, useValue : '/' },
-    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true, providers: [ClientesService] }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Clientes } from "../models/clientes";
 
-@Injectable()
+@Injectable({
+  providedIn: "root"
+})
+
 export class ClientesService {
 
   constructor() { }
