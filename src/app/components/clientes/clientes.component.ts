99a2faf8c5f12cb111ee1dff3da8824ca30bef54
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 15:59:02 -0300

    1 file modified

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -18,7 +18,7 @@ export class ClientesComponent implements OnInit {
     C: "(Consultar)",
     L: "(Listado)"
   };
-  AccionABMC = "L"; // inicialmente inicia en el listado de paises (buscar con parametros)
+  AccionABMC = "L"; // inicialmente inicia en el listado de articulos (buscar con parametros)
   Mensajes = {
     SD: " No se encontraron registros...",
     RD: " Revisar los datos ingresados..."
@@ -27,6 +27,7 @@ export class ClientesComponent implements OnInit {
   Lista: Clientes[] = [];
   RegistrosTotal: number;
   SinBusquedasRealizadas = true;
+  
 
   Pagina = 1; // inicia pagina 1
 
@@ -36,50 +37,58 @@ export class ClientesComponent implements OnInit {
 
   constructor(
     public formBuilder: FormBuilder,
-    private ClientesService: ClientesService,
+    //private articulosService: MockArticulosService,
+    //private articulosFamiliasService: MockArticulosFamiliasService,
+    private ventas: ClientesService,
     private modalDialogService: ModalDialogService
   ) {}
 
   ngOnInit() {
-    //Aqui poner los datos de angular que sacaste
+    this.FormFiltro = this.formBuilder.group({
+      Nombre: [""],
+      Activo: [true]
+    });
+    this.FormReg = this.formBuilder.group({
+      IdVenta: [0],
+      IdCliente: [0],
+      FechaNacimiento: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}"
+          )
+        ]
+      ],
+      //Total: [null, [Validators.required, Validators.pattern("[0-9]{1,8}")]],
+      Nombre: [
+        "",
+        [Validators.required, Validators.minLength(1), Validators.maxLength(50)]
+      ],
+    });
   }
+
 
   Agregar() {
     this.AccionABMC = "A";
-    //this.FormReg.reset({ Activo: true });
+    this.FormReg.reset({ Activo: true });
     this.submitted = false;
+    //this.FormReg.markAsPristine();
     this.FormReg.markAsUntouched();
   }
 
+  // Buscar segun los filtros, establecidos en FormReg
   Buscar() {
     this.SinBusquedasRealizadas = false;
-    this.ClientesService.get().subscribe((res: any) => {
-      this.Lista = res;
-      this.RegistrosTotal = res.RegistrosTotal;
-    });
+    this.ventas
+      .get( )
+      .subscribe((res: any) => {
+        this.Lista = res;
+      });
   }
 
-  BuscarPorId(Dto, AccionABMC) {
-    window.scroll(0, 0); // ir al incio del scroll
 
-    this.ClientesService.getById(Dto.IdCliente).subscribe((res: any) => {
-      this.FormReg.patchValue(res);
-
-      /*
-      //formatear fecha de  ISO 8061 a string dd/MM/yyyy
-      var arrFecha = res.IdCliente.substr(0, 10).split("-");
-      this.FormReg.controls.IdCliente.patchValue(
-        arrFecha[2] + "/" + arrFecha[1] + "/" + arrFecha[0]
-      );*/
-
-      this.AccionABMC = AccionABMC;
-    });
-  }
-
-  Consultar(Dto) {
-    this.BuscarPorId(Dto, "C");
-  }
-
+  // grabar tanto altas como modificaciones
   Grabar() {
     this.submitted = true;
     // verificar que los validadores esten OK
@@ -90,27 +99,28 @@ export class ClientesComponent implements OnInit {
     //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
     const itemCopy = { ...this.FormReg.value };
 
-    /*
     //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
-    var arrFecha = itemCopy.FechaNacimiento.substr(0, 10).split("/");
+    var arrFecha = itemCopy.Fecha.substr(0, 10).split("/");
     if (arrFecha.length == 3)
-      itemCopy.FechaNacimiento = new Date(
+      itemCopy.Fecha = new Date(
         arrFecha[2],
         arrFecha[1] - 1,
         arrFecha[0]
       ).toISOString();
-*/
+
     // agregar post
-    if (itemCopy.IdCliente == 0 || itemCopy.IdCliente == null) {
-      itemCopy.IdCliente = 0;
-      this.ClientesService.post(itemCopy).subscribe((res: any) => {
+    if (itemCopy.IdVenta == 0 || itemCopy.IdVenta == null) {
+      itemCopy.IdVenta = 0;
+      this.ventas.post(itemCopy).subscribe((res: any) => {
         this.Volver();
         this.modalDialogService.Alert("Registro agregado correctamente.");
         this.Buscar();
       });
-    }
+    } 
   }
 
+
+  // Volver desde Agregar/Modificar
   Volver() {
     this.AccionABMC = "L";
   }
